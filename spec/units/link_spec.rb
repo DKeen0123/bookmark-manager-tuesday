eb7e89d8777639973b9bcd2931df60d7944b9823
Daniel Keen <keendaniel01@gmail.com>
2018-02-13 16:54:41 +0000

    Added unit test for adding links

--- spec/units/link_spec.rb
+++ spec/units/link_spec.rb
@@ -4,4 +4,9 @@ describe Link do
   it "returns all of the links" do
     expect(described_class.all).to include 'http://www.google.com' && 'http://www.twitter.com'
   end
+
+  it 'adds a new link to the database' do
+    described_class.add("http://www.facebook.com")
+    expect(described_class.all).to include 'http://www.facebook.com'
+  end
 end
